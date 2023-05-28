16d77e61df7f1d725d3237093ef9cd2b28419005
David Peguero <david.peguero.cancel@gmail.com>
2023-05-28 13:02:19 -0400

    Add col structure to the portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,16 @@
-export default function Portfolio(){
+import ProjectCard from "./ProjectCard/ProjectCard"
+
+export default function Portfolio() {
     return (
-        <div>
-            This is Portfolio
+        <div className="container my-5 mx-6">
+            <div className="columns">
+                <div className="row align-items-start">
+                    <ProjectCard projectName="Mental Oasis" projectImg="" projectTools=""></ProjectCard>
+                    <ProjectCard projectName="Mental Oasis" projectImg="" projectTools=""></ProjectCard>
+                </div>
+            </div>
+
         </div>
+
     )
 }
---
+++ src/components/Portfolio/ProjectCard/ProjectCard.js
@@ -0,0 +1,7 @@
+export default function ProjectCard({projectName, projectImg, projectTools}){
+    return(
+        <div className="col-6">
+            {projectName}
+        </div>
+    )
+}
