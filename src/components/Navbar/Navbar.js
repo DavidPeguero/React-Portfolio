9245a8a22eef1f73032e504910d930fdc0b112de
David Peguero <david.peguero.cancel@gmail.com>
2023-05-28 13:04:54 -0400

    Change font color to white in the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,18 @@ export default function Navbar() {
     return (
         <>
             <nav class="navbar navbar-expand-lg bg-primary">
-                <div class="container-fluid">
-                    <div class="navbar-brand">David Peguero</div>
+                <div class="container-fluid ">
+                    <div class="navbar-brand text-light">David Peguero</div>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav me-auto mb2 mb-lg-0 d-flex">
-                            <Link to='/' ><li className="nav-item"><div className="nav-link">About Me</div></li></Link>
-                            <Link to='/portfolio'><li className="nav-item" href="/portfolio"><div className="nav-link">Portfolio</div></li></Link>
-                            <Link to='/contact' ><li className="nav-item"><div className="nav-link">Contact</div></li></Link>
-                            <Link to='/resume' ><li className="nav-item"><div className="nav-link">Resume</div></li></Link>
+                            <Link to='/' ><li className="nav-item"><div className="nav-link text-light">About Me</div></li></Link>
+                            <Link to='/portfolio'><li className="nav-item" href="/portfolio"><div className="nav-link text-light">Portfolio</div></li></Link>
+                            <Link to='/contact' ><li className="nav-item"><div className="nav-link text-light">Contact</div></li></Link>
+                            <Link to='/resume' ><li className="nav-item"><div className="nav-link text-light">Resume</div></li></Link>
                         </ul>
                     </div>
                 </div>
