7fbb89b6570523893283e7eedce664a3abd390cb
David Peguero <david.peguero.cancel@gmail.com>
2023-05-28 12:13:13 -0400

    Added Blank avatar img

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,11 @@
 export default function About(){
     return(
         <div className="container my-5 mx-6">
-            <h2 className="h2">About Me</h2>
+            <div>
+                <h2 className="h2">About Me</h2>
+                <img src="./assets/images/Blank-Avatar.png" alt="Blank Avatar" style={{ maxWidth : "200px"}} className="image-fluid"/>
+                
+            </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio provident enim, unde soluta reiciendis vero perferendis iure labore voluptates ratione excepturi est explicabo expedita culpa distinctio modi aperiam repellendus, necessitatibus accusamus laboriosam suscipit! Eum earum non enim, eaque officia quibusdam voluptates consequuntur dolorum ab et delectus consectetur nam minus ad!</p>
         </div>
     )
