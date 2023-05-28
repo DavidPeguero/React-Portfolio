4225b884540641f77eded2a3860d1620e1158e32
David Peguero <david.peguero.cancel@gmail.com>
2023-05-28 11:52:09 -0400

    Remade navbar using Bootstrap

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,9 @@
-import Button from '@mui/material/Button'
+
+
+
 
 export default function About(){
-
     return(
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dolore sint dolorum excepturi fugit distinctio. Inventore natus porro corrupti quidem?
-            <Button variant="contained">Wowee</Button>
-        </div>
-        
+        <div className="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio provident enim, unde soluta reiciendis vero perferendis iure labore voluptates ratione excepturi est explicabo expedita culpa distinctio modi aperiam repellendus, necessitatibus accusamus laboriosam suscipit! Eum earum non enim, eaque officia quibusdam voluptates consequuntur dolorum ab et delectus consectetur nam minus ad!</div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,53 @@
 import { Outlet, Link } from "react-router-dom"
-import { AppBar, Typography, Button, Toolbar, Stack} from "@mui/material"
-import Menu from '@mui/material/Menu';
-import './style.css'
 
 export default function Navbar() {
     return (
         <>
-            <AppBar position="static" className="appbar" sx={{
+            {/* <AppBar position="static" className="appbar" sx={{
                 height : '100px',
                 justifyContent : 'center'
             }}>
                 <Toolbar variant="dense">
                     <Typography variant="h6" color="inherit" component="div" sx={{
-                        flexGrow : 1
+                        flexGrow : 1,
+                        fontSize : 'clamp(.75rem, 5vw, 1.25rem)'
                     }}>
                         David Peguero
                     </Typography>
                     <Stack direction="row" spacing={2} sx={{
-                        flexGrow : 1
+                        flexGrow : 1,
+                        fontSize : 'clamp(.75rem, 5vw, 1.25rem)'
                     }}>
                         <Link to='/' className="nav-link"><Button color="inherit">Home</Button></Link>
                         <Link to='/portfolio' className="nav-link"><Button color="inherit">Portfolio</Button></Link>
                         <Link to='/about' className="nav-link"><Button color="inherit">About</Button></Link>
-                        <Link to='/contact' className="nav-link"><Button color="inherit">Contact Me</Button></Link>
+                        <Link to='/contact' className="nav-link"><Button color="inherit" className="">Contact Me</Button></Link>
                     </Stack>
                 </Toolbar>
-            </AppBar>
-            <Outlet />
-            {/* <nav>
-                
-                <h1>David Peguero</h1>  
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
+            </AppBar> */}
+            <nav class="navbar navbar-expand-lg bg-primary">
+                <div class="container-fluid">
+                    <div class="navbar-brand">David Peguero</div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb2 mb-lg-0 d-flex">
+                            <Link to='/'><li className="nav-item" ><div className="nav-link active" aria-current="page">Home</div></li></Link>
+                            <Link to='/portfolio'><li className="nav-item" href="/portfolio"><div className="nav-link">Portfolio</div></li></Link>
+                            <Link to='/about' ><li className="nav-item"><div className="nav-link">About</div></li></Link>
+                            <Link to='/contact' ><li className="nav-item"><div className="nav-link">Contact</div></li></Link>
+                        </ul>
+                    </div>
+                </div>
+
+
             </nav>
 
-            <Outlet /> */}
+            <Outlet />
+
+
         </>
     )
 }
