1f956c09cde14c609987a88bf4dc5dd43daf3ea5
David Peguero <david.peguero.cancel@gmail.com>
2023-05-29 19:53:20 -0400

    Added format to add link to portfolio items

--- src/components/Portfolio/ProjectCard/ProjectCard.js
+++ src/components/Portfolio/ProjectCard/ProjectCard.js
@@ -1,19 +1,21 @@
 import './style.css'
 
-export default function ProjectCard({ projectName, projectImg, projectTools }) {
+export default function ProjectCard({ projectName, projectImg, projectTools, projectLink }) {
     return (
         <div className="col-lg-5 col-sm-12 p-card my-5 project-img">
-            <div style={{
-                backgroundImage: `url(${projectImg})`,
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                height: '200px'
-            }}  >
-                <div className="card-overlay p-3">
-                    <p>{projectName}</p>
-                    <p>{projectTools}</p>
+            <a href={projectLink}>
+                <div style={{
+                    backgroundImage: `url(${projectImg})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    height: '200px'
+                }}  >
+                    <div className="card-overlay p-3" style={{color : 'black'}}>
+                        <p>{projectName}</p>
+                        <p>{projectTools}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     )
 }
