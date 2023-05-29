1c6a74b2ff6b216c4402deb8c946c47e531933f2
David Peguero <david.peguero.cancel@gmail.com>
2023-05-29 11:53:14 -0400

    Layed Out Portfolio Page a bit better

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,12 +1,14 @@
 import ProjectCard from "./ProjectCard/ProjectCard"
 
+
 export default function Portfolio() {
     return (
         <div className="container my-5 mx-6">
-            <div className="columns">
-                <div className="row align-items-start">
-                    <ProjectCard projectName="Mental Oasis" projectImg="" projectTools=""></ProjectCard>
-                    <ProjectCard projectName="Mental Oasis" projectImg="" projectTools=""></ProjectCard>
+            <div className="columns ">
+                <div className="row d-flex align-items-start gx-6 my-5 p-container">
+                    <ProjectCard projectName="Mental Oasis" projectImg="./assets/images/Blank-Avatar.png" projectTools="MERN Stack"></ProjectCard>
+                    <ProjectCard projectName="Mental Oasis" projectImg="./assets/images/Blank-Avatar.png" projectTools="React"></ProjectCard>
+                    <ProjectCard projectName="Mental Oasis" projectImg="./assets/images/Blank-Avatar.png" projectTools=""></ProjectCard>
                 </div>
             </div>
 
--- src/components/Portfolio/ProjectCard/ProjectCard.js
+++ src/components/Portfolio/ProjectCard/ProjectCard.js
@@ -1,7 +1,17 @@
+import './style.css'
+
 export default function ProjectCard({projectName, projectImg, projectTools}){
     return(
-        <div className="col-6">
-            {projectName}
+        <div className="col-lg-5 col-sm-12 p-card">
+            <div style={{
+                backgroundImage : `url(${projectImg})`,
+                backgroundSize : 'contain',
+                backgroundRepeat : 'no-repeat',
+                height : '200px'
+            }}  >
+                <p>{projectName}</p>
+                <p>{projectTools}</p>
+            </div>
         </div>
     )
 }
