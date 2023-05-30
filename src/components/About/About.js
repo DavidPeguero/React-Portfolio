ceb10b3fcf8906ac3967042d749a8e7336c0d1ed
David Peguero <david.peguero.cancel@gmail.com>
2023-05-30 10:24:10 -0400

    Added about me description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,12 +4,19 @@
 
 export default function About(){
     return(
-        <div className="container my-5 mx-6">
+        <div className="container my-5 mx-6 d-flex flex-column align-items-center">
             <div>
                 <h2 className="h2">About Me</h2>
-                <img src="./assets/images/me.png" alt="Blank Avatar" style={{ maxWidth : "200px", maxHeight:"200px"}} className="image-fluid"/>
+                <img src={process.env.PUBLIC_URL + "/assets/images/me.png"} alt="Blank Avatar" style={{ maxWidth : "200px", maxHeight:"200px"}} className="image-fluid"/>
             </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio provident enim, unde soluta reiciendis vero perferendis iure labore voluptates ratione excepturi est explicabo expedita culpa distinctio modi aperiam repellendus, necessitatibus accusamus laboriosam suscipit! Eum earum non enim, eaque officia quibusdam voluptates consequuntur dolorum ab et delectus consectetur nam minus ad!</p>
+            <p>I am aspiring Full stack Developer who has experience in making websites using React, Javascript, HTML5, CSS, and SQL/noSQL databases.
+                I am also experience in using Python, Java, C#, and C++. I have recently completed my Certification through the UCF Fulltime Coding Bootcamp
+                and have furthered my skills in the area.
+            </p>
+            <br></br>
+            <p>Eventually I have aspirations of being able to design elegant websites both in appearance and functionality. I also
+                want to become a game developer as one of my main passions is and has always been gaming.
+            </p>
             
         </div>
     )
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,10 +6,10 @@ export default function Portfolio() {
         <section className="container my-5 mx-6">
             <div className="columns ">
                 <div className="row d-flex align-items-start gx-6 my-5 p-container justify-content-between">
-                    <ProjectCard projectName="Mental Oasis" projectImg={process.env.PUBLIC_URL+ "/assets/images/mental-oasis.png"} projectTools="Node.js | Express.js | mySQL"></ProjectCard>
-                    <ProjectCard projectName="YuGiOh Forbidden List Maker" projectImg={process.env.PUBLIC_URL + "/assets/images/ygo.png"} projectTools="Python | APIs | Webscraping"></ProjectCard>
-                    <ProjectCard projectName="Tenzies" projectImg={process.env.PUBLIC_URL + "/assets/images/tenzies.png"} projectTools="React"></ProjectCard>
-                    <ProjectCard projectName="dis(Cover) Music" projectImg={process.env.PUBLIC_URL + "/assets/images/discover-main.png"} projectTools="JS | HTML | RESTful APIs | CSS"></ProjectCard>
+                    <ProjectCard projectName="Mental Oasis" projectLink={"https://github.com/DavidPeguero/Mental-Oasis"} projectImg={process.env.PUBLIC_URL+ "/assets/images/mental-oasis.png"} projectTools="Node.js | Express.js | mySQL"></ProjectCard>
+                    <ProjectCard projectName="YuGiOh Forbidden List Maker" projectLink={"https://github.com/DavidPeguero/Yugioh-BanListChangeScraper/releases/tag/1.0.0"} projectImg={process.env.PUBLIC_URL + "/assets/images/ygo.png"} projectTools="Python | APIs | Webscraping"></ProjectCard>
+                    <ProjectCard projectName="Tenzies" projectLink={"https://david-peguero-tenzies.netlify.app/"} projectImg={process.env.PUBLIC_URL + "/assets/images/tenzies.png"} projectTools="React"></ProjectCard>
+                    <ProjectCard projectName="dis(Cover) Music" projectLink={"https://github.com/CoolJH2K/dis-cover-music"} projectImg={process.env.PUBLIC_URL + "/assets/images/discover-main.png"} projectTools="JS | HTML | RESTful APIs | CSS"></ProjectCard>
                 </div>
             </div>
         </section>
