f5ce2d989136d4a7df5448aba970ae3ed5bd8c76
David Peguero <david.peguero.cancel@gmail.com>
2023-05-31 13:25:37 -0400

    Added to more projects to the portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -10,8 +10,8 @@ export default function Portfolio() {
                     <ProjectCard projectName="YuGiOh Forbidden List Maker" projectLink={"https://github.com/DavidPeguero/Yugioh-BanListChangeScraper/releases/tag/1.0.0"} projectImg={process.env.PUBLIC_URL + "/assets/images/ygo.png"} projectTools="Python | APIs | Webscraping"></ProjectCard>
                     <ProjectCard projectName="Tenzies" projectLink={"https://david-peguero-tenzies.netlify.app/"} projectImg={process.env.PUBLIC_URL + "/assets/images/tenzies.png"} projectTools="React"></ProjectCard>
                     <ProjectCard projectName="dis(Cover) Music" projectLink={"https://github.com/CoolJH2K/dis-cover-music"} projectImg={process.env.PUBLIC_URL + "/assets/images/discover-main.png"} projectTools="JS | HTML | RESTful APIs | CSS"></ProjectCard>
-                    <ProjectCard projectName="dis(Cover) Music" projectLink={"https://github.com/CoolJH2K/dis-cover-music"} projectImg={process.env.PUBLIC_URL + "/assets/images/discover-main.png"} projectTools="JS | HTML | RESTful APIs | CSS"></ProjectCard>
-                    <ProjectCard projectName="dis(Cover) Music" projectLink={"https://github.com/CoolJH2K/dis-cover-music"} projectImg={process.env.PUBLIC_URL + "/assets/images/discover-main.png"} projectTools="JS | HTML | RESTful APIs | CSS"></ProjectCard>
+                    <ProjectCard projectName="Dialogue Dash" projectLink={"https://github.com/CWolfe1320/dialoguedash"} projectImg={process.env.PUBLIC_URL + "/assets/images/dd-img.gif"} projectTools="Unity | C# | Wit.ai"></ProjectCard>
+                    <ProjectCard projectName="JATE" projectLink={"https://github.com/DavidPeguero/JATE"} projectImg={process.env.PUBLIC_URL + "/assets/images/jate.png"} projectTools="MongoDB | Mongoose | Webpack"></ProjectCard>
                 </div>
             </div>
         </section>
